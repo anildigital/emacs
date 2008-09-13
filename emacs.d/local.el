1206b4501ad7e91fe81f238fa43500a60f94c5de
Anil Wadghule <anildigital@gmail.com>
2008-09-13 12:15:06 +0530

    My macbook emacs config files.

---
+++ emacs.d/local.el
@@ -0,0 +1,18 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+; custom place to save customizations
+(setq custom-file "~/.emacs.d/anil/custom.el")
+(load custom-file)
+
+(load ".passwords")
+
+(load "anil/global")
+(load "anil/helpers")
+(load "anil/modes")
+(load "anil/theme")
+
+(vendor 'cheat)
+(vendor 'magit)
+(vendor 'gist)
+(vendor 'growl)
+(vendor 'twittering-mode)
