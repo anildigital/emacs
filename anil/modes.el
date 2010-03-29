6aa881b40c8cbf8b5b8f9a95169931551e1a7032
Anil Wadghule <anildigital@gmail.com>
2010-03-30 00:03:03 +0530

    load theme for gui only removed duplicate code

--- anil/modes.el
+++ anil/modes.el
@@ -41,16 +41,6 @@
 (global-set-key "\C-ck" 'mode-compile-kill)
 
 
-; mode-compile
-(autoload 'mode-compile "mode-compile"
-   "Command to compile current buffer file based on the major mode" t)
-(global-set-key "\C-cc" 'mode-compile)
- 
-(autoload 'mode-compile-kill "mode-compile"
- "Command to kill a compilation launched by `mode-compile'" t)
-(global-set-key "\C-ck" 'mode-compile-kill)
-
-
 ; c
 (add-hook 'c-mode-hook
           '(lambda ()
