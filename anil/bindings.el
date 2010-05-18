a07e51c799dcee2e32810c45a480c93ccd17a9b2
Anil Wadghule <anildigital@gmail.com>
2010-05-18 16:03:02 +0530

    Added key bindings

--- anil/bindings.el
+++ anil/bindings.el
@@ -1,4 +1,4 @@
-                                        ; general
+ ; general
 (global-set-key "\C-x\C-b" 'buffer-menu)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key (kbd "A-F") 'ack)
@@ -13,29 +13,36 @@
   (define-key osx-key-mode-map (kbd "A-;")
     '(lambda () (interactive) (message "noop")))
   
-  ;; What's paper?
+ ;; What's paper?
   (define-key osx-key-mode-map (kbd "A-p")
     '(lambda () (interactive) (message "noop"))))
 
 
-                                        ; no mailing!
+ ; no mailing!
 (global-unset-key (kbd "C-x m"))
 (global-unset-key "\C-z")
 
-                                        ; vim emulation
+;; vim emulation
 (global-set-key [C-tab] 'other-window)
 
-                                        ; copy whole line
+;; copy whole line
 (global-set-key "\C-c\l" 'copy-line)
-
                                         ; swap windows
 (global-set-key (kbd "C-c s") 'swap-windows)
 
+(global-set-key (kbd "<f5>") 'eval-buffer)
 
+;; Rails source
+(global-set-key (kbd "<f9>") (lambda ()
+                                (interactive)
+                                (find-file "~/code/rails/rails")))
 ;; Fast go to .emacs.
 (global-set-key (kbd "<f8>") (lambda ()
                                 (interactive)
                                 (find-file "~/.emacs.d/emacs.el")))
+
+;; simplenote-browse mode
+(global-set-key (kbd "<f7>") 'simplenote-browse)
 
 
 ;; Increase and decrease font size.
