8ebb0a8096dfd16d083d25ba1161827c8066e52e
Anil Wadghule <anildigital@gmail.com>
2010-06-23 20:36:36 +0530

    Added zen-and-art color theme

--- anil/theme.el
+++ anil/theme.el
@@ -1,5 +1,5 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 (when window-system 
-  (load-file "~/.emacs.d/vendor/twilight-emacs.el/color-theme-twilight.el")
-  (color-theme-twilight))
+  (load-file "~/.emacs.d/vendor/zen-and-art.el")
+  (color-theme-zen-and-art))
