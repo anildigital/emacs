4057cf47c0802d6adc33421bc417a4951eba9c82
Anil Wadghule <anildigital@gmail.com>
2011-01-04 23:55:06 +0530

    Back to twlight theme.

--- anil/theme.el
+++ anil/theme.el
@@ -5,5 +5,8 @@
        `((background-color . "darkslateblue")
          (foreground-color . "lightcyan")))
 
-(load-file "~/.emacs.d/vendor/njs-wheat-color-theme.el")
-(color-theme-njs-wheat)
+;(load-file "~/.emacs.d/vendor/njs-wheat-color-theme.el")
+;(color-theme-njs-wheat)
+(load-file "~/.emacs.d/vendor/twilight-emacs.el/color-theme-twilight.el")
+(color-theme-twilight)
+
