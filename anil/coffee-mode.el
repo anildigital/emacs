caa24d01b3ce682b796c6b4996d43acdc544fb7d
Anil Wadghule <anildigital@gmail.com>
2011-03-07 23:47:45 +0530

    Misc changes in coffee-mode.el

--- anil/coffee-mode.el
+++ anil/coffee-mode.el
@@ -19,10 +19,45 @@
   ;; Riding edge.
   (setq coffee-command "/usr/local/share/npm/bin/coffee"))
 
-  ;; Compile '.coffee' files on every save
-  (add-hook 'after-save-hook
-      '(lambda ()
-         (when (string-match "\.coffee$" (buffer-name))
-          (coffee-compile-file))))
+;; Compile '.coffee' files on every save
+(add-hook 'after-save-hook
+          '(lambda ()
+             (when (string-match "\.coffee$" (buffer-name))
+               (coffee-compile-file))))
 
 (add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
+
+
+
+;; taken from defunkt's emacs config
+(defun anil-coffee-mode-hook ()
+  "anil's hacks and experiments for `coffee-mode.el'."
+
+  ;; Compile .js on every save.
+  ;;   (add-hook 'before-save-hook
+  ;;             '(lambda ()
+  ;;                (when (not (string= (buffer-name) "Cakefile"))
+  ;;                  (shell-command "cake build"))))
+
+
+
+  ;; aww yeah
+  (define-key coffee-mode-map "\C-L" 'coffee-insert-console)
+
+  ;; I like debug mode, sometimes.
+  (setq coffee-debug-mode nil))
+
+(add-hook 'coffee-mode-hook (lambda () (anil-coffee-mode-hook)))
+
+(defadvice textmate-next-line (around coffee-tm-next-line)
+  (interactive)
+  (if (string= major-mode "coffee-mode")
+      (progn
+        (end-of-line)
+        (coffee-newline-and-indent))
+    ad-do-it))
+(ad-activate 'textmate-next-line)
+
+(defun coffee-insert-console ()
+  (interactive)
+  (insert "console.log "))
