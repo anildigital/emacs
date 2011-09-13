b30ffb69136eb002d3831b723b32ded2950890f3
Anil Wadghule <anildigital@gmail.com>
2011-09-13 23:09:09 +0530

    Enable Ruby electric mode functionality

--- anil/ruby.el
+++ anil/ruby.el
@@ -13,3 +13,20 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
+
+
+;; Add this to ~/.emacs
+
+;; Missing from ruby-mode.el, see https://groups.google.com/group/emacs-on-rails/msg/565fba8263233c28
+(defun ruby-insert-end () 
+  "Insert \"end\" at point and reindent current line." 
+  (interactive) 
+  (insert "end") 
+  (ruby-indent-line t) 
+  (end-of-line)) 
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (require 'ruby-electric)
+            (ruby-electric-mode t)))
