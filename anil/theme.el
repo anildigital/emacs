3fb136b6a7030c2298e422091d19603c1d37d0b1
Anil Wadghule <anildigital@gmail.com>
2011-09-24 18:21:14 +0530

    Use twilight theme

--- anil/theme.el
+++ anil/theme.el
@@ -2,7 +2,13 @@
 (setq color-theme-is-global t)
 
 
-(add-to-list 'load-path "~/.emacs.d/vendor/emacs-color-theme-solarized/")
+; color theme solarized dark
+;(add-to-list 'load-path "~/.emacs.d/vendor/emacs-color-theme-solarized/")
 
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
+;(require 'color-theme-solarized)
+;(color-theme-solarized-dark)
+
+
+; color theme twilight 
+(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+(color-theme-twilight)
