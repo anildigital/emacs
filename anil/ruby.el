59708fb2c45926a317b927b6a4a9380e91d0dbef
Anil Wadghule <anildigital@gmail.com>
2011-12-10 11:08:23 +0530

    Enable ruby-mode for Capfile.

--- anil/ruby.el
+++ anil/ruby.el
@@ -15,6 +15,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
 
