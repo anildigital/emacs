edd93e39450d028ce3f4157a517f829f19442b04
Anil Wadghule <anildigital@gmail.com>
2017-01-25 13:30:46 +0530

    Update keybindings

--- anil/key-bindings.el
+++ anil/key-bindings.el
@@ -77,14 +77,10 @@
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 ;; neotree toggle
-(global-set-key (kbd "s-\\") 'neotree-toggle)
+(global-set-key (kbd "s-\\") 'neotree-projectile-action)
 
 ;; helm M-x
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key "\C-x\C-m" 'helm-M-x)
-(global-set-key "\C-xm" 'helm-M-x)
-
-(global-set-key "\C-c\C-m" 'helm-M-x)
 
 ;; browse kill ring
 (global-set-key "\C-cy" 'browse-kill-ring)
