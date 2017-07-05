3bc6edf059ddd02f29b9a9c64db7843ebec31073
Anil Wadghule <anildigital@gmail.com>
2017-07-05 14:56:10 +0530

    Add coffeelint check support

--- anil/modes.el
+++ anil/modes.el
@@ -278,3 +278,11 @@
 
 ;; avy
 (require 'avy)
+
+;; coffee
+(setq coffee-tab-width 2)
+(custom-set-variables
+ '(coffee-tab-width 2)
+ '(flycheck-coffeelintrc "~/.emacs.d/coffeelint.json"))
+
+(add-hook 'coffee-mode-hook 'flymake-mode)
