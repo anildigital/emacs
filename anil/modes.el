11cb97d1ea323859f947985fff2effbb4abb0d08
Anil Wadghule <anildigital@gmail.com>
2017-07-05 14:56:27 +0530

    Update markdown mode loading

--- anil/modes.el
+++ anil/modes.el
@@ -26,9 +26,14 @@
 
 ;; markdown mode
 (require 'markdown-mode)
-(add-to-list 'auto-mode-alist '(".text" . markdown-mode))
-(add-to-list 'auto-mode-alist '(".markdown" . markdown-mode))
-(add-to-list 'auto-mode-alist '(".md" . markdown-mode))
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (setq markdown-command "/usr/local/bin/markdown")
 
