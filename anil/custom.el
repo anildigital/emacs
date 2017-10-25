100ba250961eff458c3dea36414e95f7d142f78b
Anil Wadghule <anildigital@gmail.com>
2017-10-25 19:43:37 +0530

    custom.el modifications

---
+++ anil/custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2 t)
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$|beam")
+ '(flycheck-coffeelintrc "~/.emacs.d/coffeelint.json")
+ '(mixfmt-mix "/usr/local/bin/mix")
+ '(neo-hidden-regexp-list
+   (quote
+    ("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.beam$")))
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets yaml-mode wsd-mode writeroom-mode which-key web-mode use-package twittering-mode textmate sublime-themes smex smartparens smart-mode-line scpaste ruby-electric ruby-compilation robe react-snippets rainbow-mode prodigy popwin popup-imenu polymode perspective paredit paradox pallet org-bullets nyan-mode neotree multiple-cursors move-text markdown-mode magit less-css-mode js2-mode idris-mode ido-vertical-mode idle-highlight-mode helm-projectile guru-mode grizzl golden-ratio go-mode git-timemachine git-lens git-gutter gist flyspell-popup flycheck-mix flycheck-elm flycheck-elixir flycheck-credo flycheck-cask feature-mode expand-region exec-path-from-shell evil ensime enh-ruby-mode emmet-mode elm-mode elixir-yasnippets editorconfig drag-stuff dracula-theme dockerfile-mode discover-my-major dired-details dash-at-point company-tern coffee-mode circe cider chruby browse-kill-ring ag ace-window)))
+ '(projectile-globally-ignored-files (quote ("TAGS" "*\\~" "*node_modules/*"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:foreground "green3"))))
+ '(diff-removed ((t (:foreground "red3")))))
