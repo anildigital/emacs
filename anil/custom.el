730edd48af6a3b3af205bbb60423ab7bb9441fe6
Anil Wadghule <anildigital@gmail.com>
2017-11-02 13:21:08 +0530

    Update custom.el

--- anil/custom.el
+++ anil/custom.el
@@ -3,16 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2 t)
  '(flycheck-coffeelintrc "~/.emacs.d/coffeelint.json")
+ '(mixfmt-elixir "/usr/local/bin/elixir")
  '(mixfmt-mix "/usr/local/bin/mix")
  '(package-selected-packages
    (quote
     (yasnippet-snippets yaml-mode wsd-mode writeroom-mode which-key web-mode use-package twittering-mode treemacs-projectile textmate sublime-themes smex smartparens smart-mode-line scpaste ruby-electric ruby-compilation robe react-snippets rainbow-mode prodigy popwin popup-imenu polymode perspective paredit paradox pallet org-bullets nyan-mode neotree multiple-cursors move-text markdown-mode magit less-css-mode js2-mode idris-mode ido-vertical-mode idle-highlight-mode helm-projectile guru-mode grizzl golden-ratio go-mode git-timemachine git-lens git-gutter gist flyspell-popup flycheck-mix flycheck-elm flycheck-elixir flycheck-credo flycheck-cask feature-mode expand-region exec-path-from-shell evil ensime enh-ruby-mode emmet-mode elm-mode elixir-yasnippets editorconfig drag-stuff dracula-theme dockerfile-mode discover-my-major dired-details dash-at-point company-tern coffee-mode circe cider chruby browse-kill-ring browse-at-remote ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diff-added ((t (:foreground "green3"))))
- '(diff-removed ((t (:foreground "red3")))))
