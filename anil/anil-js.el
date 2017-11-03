bc7a6076ff64bbbbed97bf21351b0dd348c7918a
Anil Wadghule <anildigital@gmail.com>
2017-11-04 00:51:33 +0530

    Add tern mode for js files

--- anil/anil-js.el
+++ anil/anil-js.el
@@ -4,3 +4,13 @@
        (lambda ()
          (untabify (point-min) (point-max))))))
 (add-to-list 'auto-mode-alist '(".js" . js2-mode))
+
+
+(setq js2-strict-missing-semi-warning nil)
+(setq js2-missing-semi-one-line-override t)
+
+(require 'company-tern)
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'js2-mode-hook (lambda ()
+                           (tern-mode)
+                           (company-mode)))
