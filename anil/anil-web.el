215f4938493e7e98fa950d8f691b058c3803afb4
Anil Wadghule <anildigital@gmail.com>
2017-11-03 15:52:02 +0530

    Cleanup web mode

--- anil/anil-web.el
+++ anil/anil-web.el
@@ -1,23 +1,8 @@
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.eex?\\'" . web-mode))
-
-(add-hook 'web-mode-hook
-  (lambda ()
-    (if (equal web-mode-content-type "javascript")
-      (web-mode-set-content-type "jsx")
-      (message "now set to: %s" web-mode-content-type))))
-
-(setq web-mode-content-types-alist
-  '(("jsx" . "\\.js[x]?\\'")))
-
 
 (defun web-mode-hook ()
   "Hooks for Web mode."
@@ -28,8 +13,5 @@
 (add-hook 'web-mode-hook  'web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-(add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))   ;; JS + JSX
-
 ;; Web mode
 (define-key web-mode-map (kbd "M-n") 'web-mode-tag-match)
-
