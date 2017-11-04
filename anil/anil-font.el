3b07d5d9dc57265bd95baf1b2d346fb5696b3bc0
Anil Wadghule <anildigital@gmail.com>
2017-11-04 23:29:04 +0530

    Use source code pro programming font

--- anil/anil-font.el
+++ anil/anil-font.el
@@ -1,7 +1,7 @@
 ;;; Code:
 ;; Set favorite font
-;; (setq anil-fav-font "-*-Inconsolata-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
-;; (set-frame-font anil-fav-font)
+(setq anil-fav-font "-*-Source Code Pro-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
+(set-frame-font anil-fav-font)
 
-;; (add-to-list 'default-frame-alist (cons 'font anil-fav-font))
-;; (add-to-list 'initial-frame-alist (cons 'font anil-fav-font))
+(add-to-list 'default-frame-alist (cons 'font anil-fav-font))
+(add-to-list 'initial-frame-alist (cons 'font anil-fav-font))
