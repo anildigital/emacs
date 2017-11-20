10739ae4a85db811ed4c07911fc7ed63a9b1d964
Anil Wadghule <anildigital@gmail.com>
2017-11-20 13:10:41 +0530

    Don't need treemacs fix as it's part of Emacs now.

--- anil/anil-treemacs.el
+++ anil/anil-treemacs.el
@@ -18,19 +18,3 @@
           treemacs-never-persist              nil
           treemacs-is-never-other-window      nil
           treemacs-goto-tag-strategy          'refetch-index)
-
-
-;; TODO remove this if it gets available in official pacakge
-(defun treemacs-visit-node-no-split (&optional arg)
-  "Open current file or tag within `next-window'.
-Stay in current window with a prefix argument ARG."
-  (interactive "P")
-  (when (one-window-p)
-    (save-selected-window
-      (split-window nil nil (if (eq 'left treemacs-position) 'right 'left))))
-  (treemacs--execute-button-action
-   :file-action (find-file (treemacs--safe-button-get btn 'abs-path))
-   :dir-action (dired (treemacs--safe-button-get btn 'abs-path))
-   :tag-action (treemacs--goto-tag btn)
-   :save-window arg
-   :no-match-explanation "Node is neither a file, a directory or a tag - nothing to do here."))
