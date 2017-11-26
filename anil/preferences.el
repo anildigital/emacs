d6f1f939ef459c84827c1b4da95a962f9392a21d
Anil Wadghule <anildigital@gmail.com>
2017-11-26 12:45:35 +0530

    Don't need this with Emacs 26

--- anil/preferences.el
+++ anil/preferences.el
@@ -39,9 +39,6 @@
 (unless (display-graphic-p)
   (load-theme 'wheatgrass))
 
-;; (global-hl-line-mode 1)
-(global-linum-mode 0)
-
 (setq ediff-split-window-function 'split-window-horizontally)
 (setq ediff-diff-options "-w")
 
