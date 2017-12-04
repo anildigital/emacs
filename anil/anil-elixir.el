27b69fa9bef8150567f7d0d07bc7a058d7e98c6d
Anil Wadghule <anildigital@gmail.com>
2017-12-04 12:53:30 +0530

    Disable alchemist mode as weird test report errors

--- anil/anil-elixir.el
+++ anil/anil-elixir.el
@@ -3,15 +3,15 @@
 ;;; Code:
 
 ;; alchemist
-(require 'alchemist)
-(setq alchemist-execute-command "/usr/local/bin/elixir") ;; default: elixir
-(setq alchemist-mix-command "/usr/local/bin/mix")
-(setq alchemist-compile-command "/usr/local/bin/elixirc") ;; default: elixirc
-(setq alchemist-iex-program-name "/usr/local/bin/iex") ;; default: iex
-(setq alchemist-buffer-status-modeline nil)
-(setq alchemist-key-command-prefix (kbd "C-c ,")) ;; default: (kbd "C-c a")
-(setq alchemist-test-mode-highlight-tests nil) ;; default t
-(setq alchemist-hooks-test-on-save t)
+;; (require 'alchemist)
+;; (setq alchemist-execute-command "/usr/local/bin/elixir") ;; default: elixir
+;; (setq alchemist-mix-command "/usr/local/bin/mix")
+;; (setq alchemist-compile-command "/usr/local/bin/elixirc") ;; default: elixirc
+;; (setq alchemist-iex-program-name "/usr/local/bin/iex") ;; default: iex
+;; (setq alchemist-buffer-status-modeline nil)
+;; (setq alchemist-key-command-prefix (kbd "C-c ,")) ;; default: (kbd "C-c a")
+;; (setq alchemist-test-mode-highlight-tests nil) ;; default t
+;; (setq alchemist-hooks-test-on-save t)
 
 (require 'elixir-mode)
 
