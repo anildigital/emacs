faf5001c3fe0a288a5ce0595e06fc72b9c748f73
Anil Wadghule <anildigital@gmail.com>
2017-12-28 18:30:08 +0530

    Enable circe config

--- anil/anil-irc.el
+++ anil/anil-irc.el
@@ -1,5 +1,46 @@
+;; This defines the password variables below
+(when (file-exists-p "~/.private.el")
+  (load-file "~/.private.el"))
+
+(setq circe-network-defaults nil)
+
 (setq circe-network-options
-      `(("Freenode"
+      `(
+        ("Freenode (Elitebnc)"
+         :host "alpha.elitebnc.org"
+         :tls t
+         :port 1338
          :nick "adgtl"
-         :channels ("#emacs" "#fauna")
-         )))
+         :channels (:after-auth "#fauna" "#webrtc")
+         :nickserv-password, freenode-password
+         :pass, server-password
+         :nickserv-ghost-command "PRIVMSG NickServ :GHOST {nick} {password}"
+         :nickserv-ghost-confirmation "has been ghosted\\.$\\|is not online\\.$"
+         )
+
+        ))
+
+(setq circe-use-cycle-completion t)
+
+(setq circe-reduce-lurker-spam t)
+
+(enable-circe-color-nicks)
+
+(require 'lui-autopaste)
+(add-hook 'circe-channel-mode-hook 'enable-lui-autopaste)
+
+(add-hook 'circe-chat-mode-hook 'my-circe-prompt)
+(defun my-circe-prompt ()
+  (lui-set-prompt
+   (concat (propertize (concat (buffer-name) ">")
+                       'face 'circe-prompt-face)
+           " ")))
+
+(setq circe-format-say "{nick:-16s} {body}")
+
+(load "lui-logging" nil t)
+(enable-lui-logging-globally)
+
+
+(setq lui-track-bar-behavior 'before-switch-to-buffer)
+(enable-lui-track-bar)
