133122b9239378a189294ccab01a92728656d26a
Anil Wadghule <anildigital@gmail.com>
2018-01-17 10:02:32 +0530

    Set desktop file path

--- anil/modes.el
+++ anil/modes.el
@@ -74,14 +74,7 @@
 
 ;; desktop-save mode
 ;; Automatically save and restore sessions
-(setq desktop-dirname             "~/.emacs.d/desktop/"
-      desktop-base-file-name      "emacs.desktop"
-      desktop-base-lock-name      "lock"
-      desktop-path                (list desktop-dirname)
-      desktop-save                t
-      desktop-files-not-to-save   "^$" ;reload tramp paths
-      desktop-load-locked-desktop nil
-      desktop-auto-save-timeout   30)
+(setq desktop-path (list "~/emacs-server"))
 (desktop-save-mode 1)
 
 ;;
