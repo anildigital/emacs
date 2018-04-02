8e9bc11be53eae7f3017b5b0280dbf19d1bf8e87
Anil Wadghule <anildigital@gmail.com>
2018-04-02 15:19:55 +0530

    Add one more binding for yas-expand

--- anil/anil-yasnippet.el
+++ anil/anil-yasnippet.el
@@ -8,5 +8,6 @@
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+(define-key yas-minor-mode-map (kbd "<C-tab>") 'yas-expand)
 
 (provide 'yasnnipet)
