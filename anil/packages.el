acbe9276c3c04121cc87b13b1569659287322fc4
Anil Wadghule <anildigital@gmail.com>
2018-04-02 15:19:21 +0530

    Conditionally choose how to load cask as per OS type

--- anil/packages.el
+++ anil/packages.el
@@ -1,4 +1,7 @@
-;(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
-(add-to-list 'load-path "~/.cask/")
-(require 'cask)
+(if (eq system-type 'darwin)
+    (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+  ((add-to-list 'load-path "~/.cask/")
+   (require 'cask))
+  )
+
 (cask-initialize)
