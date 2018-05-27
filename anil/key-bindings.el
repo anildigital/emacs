34a246ce0eb164abaa1cd93942bb8bded495e2ff
Anil Wadghule <anildigital@gmail.com>
2018-05-27 14:43:04 +0530

    swiper-all keybinding

--- anil/key-bindings.el
+++ anil/key-bindings.el
@@ -46,6 +46,7 @@
 ;; Ace jump
 (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "C-'") 'swiper-avy)
+(global-set-key (kbd "C-c u") 'swiper-all)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
