347e10b56774a92d841ace2939d57058af837518
Anil Wadghule <anildigital@gmail.com>
2018-05-31 18:42:30 +0530

    Use Fira font when it's window-system/GUI

--- anil/anil-font.el
+++ anil/anil-font.el
@@ -1,7 +1,15 @@
 ;;; Code:
 ;; Set favorite font
-(setq anil-fav-font "-*-Source Code Pro-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
-(set-frame-font anil-fav-font)
+(when (not (window-system))
+  (setq anil-fav-font "-*-Source Code Pro-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
+  (set-frame-font anil-fav-font)
+  (add-to-list 'default-frame-alist (cons 'font anil-fav-font))
+  (add-to-list 'initial-frame-alist (cons 'font anil-fav-font))
+  )
 
-(add-to-list 'default-frame-alist (cons 'font anil-fav-font))
-(add-to-list 'initial-frame-alist (cons 'font anil-fav-font))
+(when (window-system)
+  (setq anil-fav-font "Fira Code")
+  (set-frame-font anil-fav-font)
+  (mac-auto-operator-composition-mode)
+  (add-to-list 'default-frame-alist (cons 'font anil-fav-font))
+  (add-to-list 'initial-frame-alist (cons 'font anil-fav-font)))
