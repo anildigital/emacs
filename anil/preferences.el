d884a24f471ae9fcf7aa5a71e653406cd6b614dd
Anil Wadghule <anildigital@gmail.com>
2018-06-02 10:20:32 +0530

    Fix fullscreen issue

--- anil/preferences.el
+++ anil/preferences.el
@@ -1,7 +1,6 @@
 ; don't show startup message.
 (setq inhibit-startup-message t)
 
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 ;(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
