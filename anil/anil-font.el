b3aea8779be8eea674035f607d8c74261695b698
Anil Wadghule <anildigital@gmail.com>
2019-06-26 20:45:53 +0530

    Set font to Inconsolata

--- anil/anil-font.el
+++ anil/anil-font.el
@@ -1,8 +1,6 @@
 ;;; Code:
 ;; Set favorite font
-;(setq anil-fav-font "-*-Source Code Pro-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-(setq anil-fav-font "-*-Inconsolata-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1")
-(set-frame-font anil-fav-font)
-
-(add-to-list 'default-frame-alist (cons 'font anil-fav-font))
-(add-to-list 'initial-frame-alist (cons 'font anil-fav-font))
+(set-face-attribute 'default nil
+                    :font "Inconsolata"
+                    :height 160
+                    :weight 'regular)
