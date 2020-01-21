9af437ac6cc7dab6232c4e3eedcd4294c138c777
Anil Wadghule <anildigital@gmail.com>
2020-01-21 16:10:04 +0530

    Add emmet mode and company auto complete for web mode

--- anil/anil-web.el
+++ anil/anil-web.el
@@ -4,14 +4,20 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.eex?\\'" . web-mode))
 
-(defun web-mode-hook ()
+(defun anil-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-indent-style 2)
+  (emmet-mode)
+  (set (make-local-variable 'company-backends) '(company-css company-web-html company-yasnippet company-files))
   )
-(add-hook 'web-mode-hook  'web-mode-hook)
+(add-hook 'web-mode-hook  'anil-web-mode-hook)
+
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(setq web-mode-enable-current-column-highlight t)
+(setq web-mode-enable-current-element-highlight t)
 
 ;; Web mode
 (define-key web-mode-map (kbd "M-n") 'web-mode-tag-match)
