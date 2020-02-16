d7f532f04429ae51d547845aa5e573f3db986199
Anil Wadghule <anildigital@gmail.com>
2020-02-16 20:52:48 +0530

    Use eglot for Elixir format of files upon save

--- anil/anil-elixir.el
+++ anil/anil-elixir.el
@@ -17,21 +17,22 @@
 (require 'elixir-mode)
 
 ;; Set elixir paths
-(setq elixir-format-elixir-path "/usr/local/bin/elixir")
-(setq elixir-format-mix-path "/usr/local/bin/mix")
+;; (setq elixir-format-elixir-path "/usr/local/bin/elixir")
+;; (setq elixir-format-mix-path "/usr/local/bin/mix")
 
 
-;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode.
 (add-hook 'elixir-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+          (lambda () (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
 
-;; elixir's .formatter.exs
-(add-hook 'elixir-format-hook (lambda ()
-                                 (if (projectile-project-p)
-                                      (setq elixir-format-arguments
-                                            (list "--dot-formatter"
-                                                  (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
-                                   (setq elixir-format-arguments nil))))
+;; ;; elixir's .formatter.exs
+;; (add-hook 'elixir-format-hook (lambda ()
+;;                                  (if (projectile-project-p)
+;;                                       (setq elixir-format-arguments
+;;                                             (list "--dot-formatter"
+;;                                                   (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
+;;                                    (setq elixir-format-arguments nil))))
+
+
 ;; Flycheck Elixir
 (require 'flycheck-elixir)
 
