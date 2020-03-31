f291311f670ba4bc1a972b43b89e7e5d23817a9b
Anil Wadghule <anildigital@gmail.com>
2020-04-01 01:38:24 +0530

    Updates

--- anil/custom.el
+++ anil/custom.el
@@ -7,10 +7,10 @@
  '(javascript-indent-level 2 t)
  '(js-indent-level 2)
  '(package-selected-packages
-   '(wsd-mode yasnippet global-so-long volatile-highlights global-so-long-mode poly-markdown volatile-highlights-mode undo-tree dimmer projectile-ripgrep ripgrep ivy-mode elisp-format browse-at-remote which-key winner winne winner-mode less-css-mode go-mode multiple-cursors move-text ace-jump-mode diff-hl guru-mode guru dockerfile-mode saveplace counsel-projectile org-projectile ido-vertical-mode yasnnipet-mode yasnnipet org-bullets yaml-mode web-mode uniquify-mode uniquify treemacs-projectile magit flyspell-mode recentf-mode recentf recent-f markdown-mode company js2-mode flyspell flyspell-popup flycheck-dialyxir flycheck-credo exunit flycheck-mix flycheck-elixir elixir-mode eglot exec-path-from-shell wakatime-mode dracula-theme use-package))
+   '(edit-indirect org-ac imenu-anywhere super-save zop-to-char anzu indium nvm graphql-mode ag restclient terraform-mode smart-mode-line erlang fish-completion helm-org-rifle ox-pandoc counsel-world-clock elixir-yasnippets yasnippet-snippets flycheck-cask ansi package-build shut-up epl git commander f dash s cask flycheck-elm paradox react-snippets feature-mode editorconfig rainbow-mode git-timemachine discover-my-major smartparens emmet-mode paredit elm-mode gist dash-at-point scpaste rbenv rubocop ruby-electric robe org git-lens ruby-compilation counsel-dash goto-chg expand-region wsd-mode yasnippet global-so-long volatile-highlights global-so-long-mode poly-markdown volatile-highlights-mode undo-tree dimmer projectile-ripgrep ripgrep ivy-mode elisp-format browse-at-remote which-key winner winne winner-mode less-css-mode go-mode multiple-cursors move-text ace-jump-mode diff-hl guru-mode guru dockerfile-mode saveplace counsel-projectile org-projectile ido-vertical-mode yasnnipet-mode yasnnipet org-bullets yaml-mode web-mode uniquify-mode uniquify treemacs-projectile magit flyspell-mode recentf-mode recentf recent-f markdown-mode company js2-mode flyspell flyspell-popup flycheck-dialyxir flycheck-credo exunit flycheck-mix flycheck-elixir elixir-mode eglot exec-path-from-shell wakatime-mode dracula-theme use-package))
  '(wakatime-python-bin nil)
- '(web-mode-enable-current-column-highlight t t)
- '(web-mode-enable-current-element-highlight t t))
+ '(web-mode-enable-current-column-highlight t)
+ '(web-mode-enable-current-element-highlight t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
