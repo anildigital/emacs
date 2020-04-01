dd3e6a26b0c8dea3bedae1e9ff86ec0ab3004485
Anil Wadghule <anildigital@gmail.com>
2020-04-02 00:55:46 +0530

    Updates

--- anil/anil-company.el
+++ anil/anil-company.el
@@ -1,8 +1,8 @@
 (use-package
   company
   :ensure t
-  :config (add-hook 'after-init-hook 'global-company-mode)
-  (add-hook 'company-mode-hook
-            (setq company-dabbrev-downcase nil))
+  :config
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-dabbrev-downcase nil)
   (add-to-list 'company-backends 'company-elm)
   (push 'company-robe company-backends))
