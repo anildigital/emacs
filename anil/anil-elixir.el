ce0374cc081c48aedc086e8302df7b84f39e6759
Anil Wadghule <anildigital@gmail.com>
2020-04-02 00:55:46 +0530

    Updates

--- anil/anil-elixir.el
+++ anil/anil-elixir.el
@@ -1,11 +1,12 @@
 ;; ;; elixir mode
-
 (use-package
   elixir-mode
   :ensure t
   :config
   (add-hook 'elixir-mode-hook 'flycheck-mode)
-  (add-hook 'elixir-mode-hook #'smartparens-mode))
+  (add-hook 'elixir-mode-hook #'smartparens-mode)
+  (add-hook 'elixir-mode-hook (lambda ()
+                                        (add-hook 'before-save-hook 'lsp-format-buffer))))
 
 (use-package
   flycheck-mix
@@ -20,10 +21,6 @@
   :diminish lsp-mode
   :hook (elixir-mode . lsp)
   :init (add-to-list 'exec-path "/Users/anil/Code/elixir-ls/release")
-  :config (defun my-elixir-mode-before-save-hook ()
-            (when (eq major-mode 'elixir-mode)
-              (lsp-format-buffer)))
-  (add-hook 'before-save-hook #'my-elixir-mode-before-save-hook)
   )
 
 (use-package
