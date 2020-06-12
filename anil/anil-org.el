b9b25ef77e72a66339e996c0e023f85dedd94c68
Anil Wadghule <anildigital@gmail.com>
2020-06-12 13:50:30 +0530

    Add org-roam

--- anil/anil-org.el
+++ anil/anil-org.el
@@ -54,3 +54,19 @@
   :ensure t
   :init (add-hook 'org-mode-hook (lambda ()
                                    (org-bullets-mode 1))))
+
+
+(use-package org-roam
+      :ensure t
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "~/org")
+      :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n j" . org-roam-jump-to-index)
+               ("C-c n b" . org-roam-switch-to-buffer)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
