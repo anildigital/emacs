3ccdf696d0c6f0c7475778b039e3500c29286ef6
Anil Wadghule <anildigital@gmail.com>
2020-06-30 20:05:26 +0530

    Update org roam config

--- anil/anil-org.el
+++ anil/anil-org.el
@@ -53,19 +53,20 @@
   :init (add-hook 'org-mode-hook (lambda ()
                                    (org-bullets-mode 1))))
 
-
 (use-package org-roam
-  :ensure t
-  :hook
-  (after-init . org-roam-mode)
-  :custom
-  (org-roam-directory "~/org")
-  (org-roam-index-file "~/org/index.org")
-  :bind (:map org-roam-mode-map
+      :ensure t
+      :hook
+      (after-init . org-roam-mode)
+      :custom
+      (org-roam-directory "~/org")
+      (org-roam-index-file "~/org/index.org")
+      :bind (:map org-roam-mode-map
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
                ("C-c n j" . org-roam-jump-to-index)
                ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph))
+               ("C-c n g" . org-roam-graph-show)
+               )
               :map org-mode-map
-              (("C-c n i" . org-roam-insert))))
+              (("C-c n i" . org-roam-insert))
+              (("C-c n I" . org-roam-insert-immediate))))
