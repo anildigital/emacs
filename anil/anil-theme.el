192fed9eea36a2e2865b33006ac5483a495be1c4
Anil Wadghule <anildigital@gmail.com>
2021-03-22 13:19:25 +0530

    Use centaur tabs

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -5,20 +5,18 @@
   :config
   (load-theme 'vscode-dark-plus t))
 
-
-(progn
-  (global-tab-line-mode t)
-  (set-face-attribute 'tab-line nil ;; background behind tabs
-                      :background "#000000"
-                      :foreground "#333333" :distant-foreground "#333333"
-                      :family "Fira Sans Condensed" :height 1.0 :box '(:line-width 6 :color "#000000" :style nil))
-  (set-face-attribute 'tab-line-tab nil ;; active tab in another window
-                      :inherit 'tab-line
-                      :foreground "#777777" :background "#111111" :box nil)
-  (set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
-                      :background "#222222" :foreground "white" :box nil)
-  (set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
-                      :background "#333333" :foreground "#bbbbbb" :box nil)
-  (set-face-attribute 'tab-line-highlight nil ;; mouseover
-                      )
-  )
+(use-package centaur-tabs
+  :ensure t
+  :demand
+  :init
+  (setq centaur-tabs-height 32)
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-style "chamfer")
+  (setq centaur-tabs-gray-out-icons 'buffer)
+  (setq centaur-tabs-set-bar 'over)
+  :config
+  (centaur-tabs-mode t)
+  (centaur-tabs-headline-match)
+  :bind
+  ("s-{" . centaur-tabs-backward)
+  ("s-}" . centaur-tabs-forward))
