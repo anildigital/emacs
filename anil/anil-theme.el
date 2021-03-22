67b0637fddd7eb054241143f07b3c0a805964794
Anil Wadghule <anildigital@gmail.com>
2021-03-22 11:41:00 +0530

    Enable tab line mode with matching VS Code theme

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -4,3 +4,21 @@
   :ensure t
   :config
   (load-theme 'vscode-dark-plus t))
+
+
+(progn
+  (global-tab-line-mode t)
+  (set-face-attribute 'tab-line nil ;; background behind tabs
+                      :background "#000000"
+                      :foreground "#333333" :distant-foreground "#333333"
+                      :family "Fira Sans Condensed" :height 1.0 :box '(:line-width 6 :color "#000000" :style nil))
+  (set-face-attribute 'tab-line-tab nil ;; active tab in another window
+                      :inherit 'tab-line
+                      :foreground "#777777" :background "#111111" :box nil)
+  (set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
+                      :background "#222222" :foreground "white" :box nil)
+  (set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
+                      :background "#333333" :foreground "#bbbbbb" :box nil)
+  (set-face-attribute 'tab-line-highlight nil ;; mouseover
+                      )
+  )
