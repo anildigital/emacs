df09774d2ab1fa7b3b874c348b6a8ef2e6d08720
Anil Wadghule <anildigital@gmail.com>
2021-03-22 16:14:22 +0530

    Add flyspell correct popup

--- anil/anil-ispell.el
+++ anil/anil-ispell.el
@@ -11,8 +11,11 @@
   (define-key flyspell-mode-map (kbd "C-.") nil)
   )
 
-
 (use-package flyspell-correct
   :ensure t
   :after flyspell
   :bind (:map flyspell-mode-map ("C-;" . flyspell-correct-wrapper)))
+
+(use-package flyspell-correct-popup
+  :ensure t
+  :after flyspell-correct)
