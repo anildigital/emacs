91ad4ce803c74be886260495687e297057e0afc2
Anil Wadghule <anildigital@gmail.com>
2021-03-29 18:57:42 +0530

    Enable icons for company

--- anil/anil-company.el
+++ anil/anil-company.el
@@ -4,4 +4,5 @@
 	:hook (after-init . global-company-mode)
   :config
   (setq company-dabbrev-downcase nil)
+	(setq company-format-margin-function #'company-vscode-light-icons-margin)
   (push 'company-robe company-backends))
