d6f840a4020ff494096d1829820e9d96afb849a8
Anil Wadghule <anildigital@gmail.com>
2021-04-14 08:04:02 +0530

    Add org roam server

--- anil/anil-org.el
+++ anil/anil-org.el
@@ -136,6 +136,22 @@
 							("C-c n l" . org-roam-buffer-toggle-display)
               ("C-c n I" . org-roam-insert-immediate))
 							))
+(use-package org-roam-server
+  :ensure t
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 6969
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20)
+  (org-roam-server-mode 1)
+  )
 
 (use-package
   ox-pandoc
