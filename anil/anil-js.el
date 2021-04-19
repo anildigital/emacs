dbd062fb8160a9264f435ab3df6e69e8c51b2eab
Anil Wadghule <anildigital@gmail.com>
2021-04-19 08:29:59 +0530

    Fix js2-mode

--- anil/anil-js.el
+++ anil/anil-js.el
@@ -1,18 +1,20 @@
 (use-package
   js2-mode
   :ensure t
-	:hook (js-mode .smartparens-mode)
-  :mode ( ("\\.js?\\'" . js2-mode))
-  :config
-  (setq js2-strict-missing-semi-warning nil)
-  (setq js2-missing-semi-one-line-override t)
-  (setq js-indent-level 2)
-  (setq-default indent-tabs-mode nil)
-  (setq javascript-indent-level 2)
-  (add-hook 'js2-mode-hook '(lambda ()
-                              (add-hook 'before-save-hook (lambda ()
-                                                            (untabify (point-min)
-                                                                      (point-max)))))))
+	:hook
+	((js-mode . smartparens-mode)
+   (before-save . (lambda ()
+                    (untabify (point-min)
+                              (point-max)))))
+
+	:mode ( ("\\.js?\\'" . js2-mode))
+	:config
+	(setq js2-strict-missing-semi-warning nil)
+	(setq js2-missing-semi-one-line-override t)
+	(setq js-indent-level 2)
+	(setq-default indent-tabs-mode nil)
+	(setq javascript-indent-level 2)
+	)
 
 (use-package
 	typescript-mode
