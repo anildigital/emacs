7308139a04a7185f7c8bd4c7176d10c0c102cf5f
Anil Wadghule <anildigital@gmail.com>
2021-10-15 06:43:49 +0530

    Change gcc path

--- early-init.el
+++ early-init.el
@@ -1,1 +1,1 @@
-(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/11:/usr/local/opt/libgccjit/lib/gcc/11:/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11.1.0")
+(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/11:/usr/local/opt/libgccjit/lib/gcc/11:/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11.2.0")
