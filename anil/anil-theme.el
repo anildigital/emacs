78c98c2ea4ee0e2fc061758ba4c1b49a31a85dd7
Anil Wadghule <anildigital@gmail.com>
2021-12-09 18:45:14 +0530

    Use dracula theme

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -1,20 +1,16 @@
 ;; theme
 
 (use-package
-  vscode-dark-plus-theme
+  dracula-theme
   :ensure t
-  :init
-  ;; Remove the border around the TODO word on org-mode files
-  (setq vscode-dark-plus-box-org-todo nil)
-
-  ;; Do not set different heights for some org faces
-  (setq vscode-dark-plus-scale-org-faces nil)
-  :config (load-theme 'vscode-dark-plus t))
+  :config
+  (load-theme 'dracula t)
+  )
 
 (use-package
   centaur-tabs
   :ensure t
-  :after (vscode-dark-plus-theme)
+  :after (dracula-theme)
   :demand
   :init (setq centaur-tabs-height 32 centaur-tabs-set-icons t centaur-tabs-set-modified-marker t
               centaur-tabs-modified-marker "*" centaur-tabs-style "chamfer"
