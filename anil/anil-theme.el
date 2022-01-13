f7e97f3c061a229be88dd509ac470f4759137f9a
Anil Wadghule <anildigital@gmail.com>
2022-01-13 21:05:40 +0530

    Revert to vscode-dark-plus theme

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -1,24 +1,30 @@
 ;; theme
-
 (use-package
-  dracula-theme
+  vscode-dark-plus-theme
   :ensure t
-  :config
-  (load-theme 'dracula t)
-  )
+  :init
+  ;; Remove the border around the TODO word on org-mode files
+  (setq vscode-dark-plus-box-org-todo nil)
+
+  ;; Do not set different heights for some org faces
+  (setq vscode-dark-plus-scale-org-faces nil)
+  :config (load-theme 'vscode-dark-plus t))
 
 (use-package
   centaur-tabs
   :ensure t
-  :after (dracula-theme)
+  :after (vscode-dark-plus-theme)
   :demand
-  :init (setq centaur-tabs-height 32 centaur-tabs-set-icons t centaur-tabs-set-modified-marker t
-              centaur-tabs-modified-marker "*" centaur-tabs-style "chamfer"
+  :init (setq centaur-tabs-height 32
+              centaur-tabs-set-icons t
+              centaur-tabs-set-modified-marker t
+              centaur-tabs-modified-marker "*"
+              centaur-tabs-style "chamfer"
               centaur-tabs-gray-out-icons 'buffer centaur-tabs-set-bar 'over
               centaur-tabs-cycle-scope 'tabs)
   :config (centaur-tabs-mode t)
-  (centaur-tabs-headline-match)
-  (centaur-tabs-group-by-projectile-project)
+  ;; (centaur-tabs-headline-match)
+  ;; (centaur-tabs-group-by-projectile-project)
   :hook (eshell-mode . centaur-tabs-local-mode)
   (org-agenda-mode . centaur-tabs-local-mode)
   :bind ("s-{" . centaur-tabs-backward)
