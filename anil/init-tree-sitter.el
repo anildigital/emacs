dfc22bf7e988399b422e9066317a816fb34c056b
Anil Wadghule <anildigital@gmail.com>
2022-01-13 21:06:11 +0530

    Update tree-sitter-mode config

--- anil/init-tree-sitter.el
+++ anil/init-tree-sitter.el
@@ -1,7 +1,10 @@
 (use-package tree-sitter
   :ensure t
+  :config
+  (tree-sitter-hl-mode)
   :hook ((elixir-mode . tree-sitter-mode))
   )
 
 (use-package tree-sitter-langs
+  :ensure t
   :after tree-sitter)
