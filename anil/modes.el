4c685f936cf86522b5bc3befee1abf7d143e28e3
Anil Wadghule <anildigital@gmail.com>
2022-01-19 21:43:24 +0530

    Update poly elixir web mode config

--- anil/modes.el
+++ anil/modes.el
@@ -116,37 +116,25 @@
 
 
 ;; Assumes web-mode and elixir-mode are already set up
-;;
 (use-package
-  polymode
+   polymode
   :ensure t
-  :mode ("\.ex$" . poly-elixir-web-mode)
-  :config (define-hostmode poly-elixir-hostmode
-            :mode 'elixir-mode)
-  (define-innermode poly-liveview-expr-elixir-innermode
-    :mode 'web-mode
-    :head-matcher (rx line-start (* space) "~L" (= 3 (char "\"'")) line-end)
-    :tail-matcher (rx line-start (* space)
-                      (= 3 (char "\"'")) line-end)
-    :head-mode 'host
-    :tail-mode 'host
-    :allow-nested nil
-    :keep-in-mode 'host
-    :fallback-mode 'host)
+  :mode ("\\.ex\\'" . poly-elixir-web-mode)
+  :init (setq web-mode-engines-alist '(("elixir" . "\\.ex\\'")))
+  :config
+  (define-hostmode poly-elixir-hostmode :mode 'elixir-mode)
   (define-innermode poly-surface-expr-elixir-innermode
     :mode 'web-mode
     :head-matcher (rx line-start (* space) "~F" (= 3 (char "\"'")) line-end)
-    :tail-matcher (rx line-start (* space)
-                      (= 3 (char "\"'")) line-end)
+    :tail-matcher (rx line-start (* space) (= 3 (char "\"'")) line-end)
     :head-mode 'host
     :tail-mode 'host
     :allow-nested nil
     :keep-in-mode 'host
     :fallback-mode 'host)
-  (define-polymode
-    poly-elixir-web-mode
+  (define-polymode poly-elixir-web-mode
     :hostmode 'poly-elixir-hostmode
-    :innermodes '(poly-liveview-expr-elixir-innermode poly-surface-expr-elixir-innermode)))
+    :innermodes '(poly-surface-expr-elixir-innermode)))
 
 (setq web-mode-engines-alist '(("elixir" . "\\.ex\\'")))
 
