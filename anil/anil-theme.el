060ce8df3316ffa1736239ba6cb1d0f05c81ae45
Anil Wadghule <anildigital@gmail.com>
2022-05-09 18:46:07 +0530

    Update colors

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -8,7 +8,12 @@
 
   ;; Do not set different heights for some org faces
   (setq vscode-dark-plus-scale-org-faces nil)
-  :config (load-theme 'vscode-dark-plus t))
+  :config
+  (load-theme 'vscode-dark-plus t)
+  (custom-set-faces
+   '(mode-line ((t (:background "turquoise3" :foreground "#fafafa" :weight normal))))
+   '(mode-line-inactive ((t (:background "turquoise4" :foreground "#d4d4d4" :weight normal)))))
+  )
 
 (use-package
   centaur-tabs
