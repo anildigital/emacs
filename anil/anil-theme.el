9312316078f870f839d0ce57efd613507d998bf3
Anil Wadghule <anildigital@gmail.com>
2022-07-18 18:19:21 +0530

    Disable centaur tabs

--- anil/anil-theme.el
+++ anil/anil-theme.el
@@ -14,25 +14,3 @@
    '(mode-line ((t (:background "turquoise3" :foreground "#fafafa" :weight normal))))
    '(mode-line-inactive ((t (:background "turquoise4" :foreground "#d4d4d4" :weight normal)))))
   )
-
-(use-package
-  centaur-tabs
-  :straight t
-  :after (vscode-dark-plus-theme)
-  :demand
-  :init (setq centaur-tabs-height 32
-              centaur-tabs-set-icons t
-              centaur-tabs-set-modified-marker t
-              centaur-tabs-modified-marker "*"
-              centaur-tabs-style "chamfer"
-              centaur-tabs-gray-out-icons 'buffer centaur-tabs-set-bar 'over
-              centaur-tabs-cycle-scope 'tabs)
-  :config (centaur-tabs-mode t)
-  ;; (centaur-tabs-headline-match)
-  ;; (centaur-tabs-group-by-projectile-project)
-  :hook
-  ((eshell-mode . centaur-tabs-local-mode)
-   (dired-mode . centaur-tabs-local-mode))
-  (org-agenda-mode . centaur-tabs-local-mode)
-  :bind ("s-{" . centaur-tabs-backward)
-  ("s-}" . centaur-tabs-forward))
