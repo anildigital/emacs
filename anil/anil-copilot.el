dbd7cc8a4e40581e86abfdf4152247c344d016df
Anil Wadghule <anildigital@gmail.com>
2022-10-07 10:56:37 +0530

    Update copilot mode config

--- anil/anil-copilot.el
+++ anil/anil-copilot.el
@@ -1,10 +1,10 @@
 (use-package copilot
   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :config (global-copilot-mode)
+  :config (with-eval-after-load 'company
+  ;; disable inline previews
+    (delq 'company-preview-if-just-one-frontend company-frontends))
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-         ("RET" . #'copilot-accept-completion)
          ("<tab>" . #'copilot-accept-completion-by-line)
-         ("C-<tab>" . #'copilot-accept-completion-by-word)
-         ("C-n" . #'copilot-next-completion)
-         ("C-p" . #'copilot-previous-completion)))
+         ("TAB" . #'copilot-accept-completion)
+         ))
