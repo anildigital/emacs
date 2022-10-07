84b5d640c634f5baa59da7c0a5cd17dab2c6218e
Anil Wadghule <anildigital@gmail.com>
2022-10-07 10:59:29 +0530

    Add more functions

--- anil/defuns.el
+++ anil/defuns.el
@@ -18,6 +18,39 @@
   (move-end-of-line 1)
   (electric-newline-and-maybe-indent))
 
+
+(defun anil-align-to-equals (begin end)
+  "Align region to equal signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) "=") 1 1 ))
+
+(defun anil-align-to-colon (begin end)
+  "Align region to colon"
+  (interactive "r")
+  (align-regexp begin end
+                (rx ":" (group (zero-or-more (syntax whitespace))) ) 1 1 ))
+
+(defun anil-align-to-comma (begin end)
+  "Align region to comma  signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx "," (group (zero-or-more (syntax whitespace))) ) 1 1 ))
+
+
+(defun anil-align-to-hash (begin end)
+  "Align region to hash ( => ) signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) "=>") 1 1 ))
+
+;; work with this
+(defun anil-align-to-comma-before (begin end)
+  "Align region to equal signs"
+  (interactive "r")
+  (align-regexp begin end
+                (rx (group (zero-or-more (syntax whitespace))) ",") 1 1 ))
+
 (defun anil-clean-slate ()
   "Kills all buffers except *scratch*"
   (interactive)
