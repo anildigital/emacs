1281c9575470f4033e36c9a0c19c1caa7cd06745
Anil Wadghule <anildigital@gmail.com>
2022-10-24 15:37:43 +0530

    Add yasnippet-snippets package

--- anil/anil-yasnippet.el
+++ anil/anil-yasnippet.el
@@ -12,3 +12,9 @@
   (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
   (define-key yas-minor-mode-map (kbd "<C-return>") 'yas-expand)
   )
+
+(use-package
+  yasnippet-snippets
+  :ensure t
+  )
+
