1ba90531cd1a3587124d4262236f034b832b4ade
Anil Wadghule <anildigital@gmail.com>
2022-11-08 23:14:50 +0530

    Disable keybinding

--- anil/anil-projectile.el
+++ anil/anil-projectile.el
@@ -7,7 +7,6 @@
   (elixir-mode . counsel-projectile-mode)
   :commands (projectile-find-file projectile-switch-project)
   :bind
-  ("s-e" . projectile-recentf)
   ("C-x 5 k" . anil/projectile-kill-non-project-buffers)
   :bind-keymap
   ("s-p" . projectile-command-map)
