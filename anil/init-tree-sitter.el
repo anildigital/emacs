a1240f78ea4d962e8903140c6f14921e0869c137
Anil Wadghule <anildigital@gmail.com>
2023-05-18 19:20:19 +0530

    Comment out packages

--- anil/init-tree-sitter.el
+++ anil/init-tree-sitter.el
@@ -1,10 +1,10 @@
-(use-package tree-sitter
-  :ensure t
-  :config
-  (tree-sitter-hl-mode)
-  :hook ((elixir-ts-mode . tree-sitter-mode))
-  )
+;; (use-package tree-sitter
+;;   :ensure t
+;;   :config
+;;   (tree-sitter-hl-mode)
+;;   :hook ((elixir-ts-mode . tree-sitter-mode))
+;;   )
 
-(use-package tree-sitter-langs
-  :ensure t
-  :after tree-sitter)
+;; (use-package tree-sitter-langs
+;;   :ensure t
+;;   :after tree-sitter)
