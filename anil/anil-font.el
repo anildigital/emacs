431d99e96b923a7ebb580e1e556ee2efd6d9b13e
Anil Wadghule <anildigital@gmail.com>
2024-12-23 17:46:37 +0530

    fix: Update font

--- anil/anil-font.el
+++ anil/anil-font.el
@@ -1,6 +1,3 @@
-;;; Code:
+
 ;; Set favorite font
-(set-face-attribute 'default nil
-                    :font "MonoLisa"
-                    :height 140
-                    :weight 'regular)
+(set-frame-font "Monolisa 14" nil t)
